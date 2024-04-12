e5662f1f6e1ccad4157da71fc659a1c7252dc25f
pollostrazon <irockpol@gmail.com>
2024-04-12 16:06:10 +0200

    FIX - mean dyn added

--- pFREYA_instrument_comms/config.py
+++ pFREYA_instrument_comms/config.py
@@ -177,10 +177,10 @@ def config(channel: str, n_steps: int, cfg_bits: list) -> None:
             max_current = 1e-6 # A
             corr_fact = 1 #105.8/103.2
     match shap_bits:
-        case [1,0]: peaking_time = 432 # ns # in line with theory
-        case [0,0]: peaking_time = 261 # ns 234 # ns
-        case [0,1]: peaking_time = 343 # ns 332 # ns
-        case [1,1]: peaking_time = 535 # ns # in line with theory
+        case [1,0]: peaking_time = 405 # ns (from mean dyn), 432 ns (from max dyn), 432 ns (from theory)
+        case [0,0]: peaking_time = 220 # ns (from mean dyn), 261 ns (from max dyn), 234 ns (from theory)
+        case [0,1]: peaking_time = 308 # ns (from mean dyn), 343 ns (from max dyn), 332 ns (from theory)
+        case [1,1]: peaking_time = 493 # ns (from mean dyn), 535 ns (from max dyn), 535 ns (from theory)
 
     current_lev = -1 * np.linspace(min_current,max_current,n_steps)
     iinj_int = current_lev * (T/2-t_r) * N_pulses + offset_charge
