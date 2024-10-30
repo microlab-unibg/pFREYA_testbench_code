51495fdb1465af00dbc69c6ad1716b9002e7982b
pollostrazon <irockpol@gmail.com>
2024-10-30 17:22:47 +0100

    update gui_test

--- pFREYA_tester/gui_test.py
+++ pFREYA_tester/gui_test.py
@@ -2,25 +2,11 @@ import tkinter as tk
 import subprocess
 import sys
 import os
-"""
-{
-    "python.analysis.extraPaths": ["../pFREYA_tester/pFREYA_tester_processing"]
-}
-"""
-# Cambia la directory passo passo
-parent_path = os.path.dirname(os.getcwd())#directory padre
-os.chdir(parent_path)
-# Entra nella directory pFREYA_tester
-a=os.chdir('pFREYA_tester')
-# Aggiungi il percorso del modulo pFREYA_tester_processing
-#processing_path = os.path.join(os.getcwd(), 'pFREYA_tester_processing')
-sys.path.append(parent_path)
-# Verifica l'importazione
-print("Modulo importato con successo:", parent_path)
+
 
 import pFREYA_tester_processing as pYtp
 
-
+gui = tk.Tk()
 
 def run_script():
   pYtp.send_clocks(gui)
@@ -30,7 +16,7 @@ def run_script():
 #metodo per shap
 
 #finestra
-gui = tk.Tk()
+
 gui.title("test 1")
 gui.geometry("400x120")
 gui.resizable(False,False)
@@ -51,7 +37,6 @@ button2.grid(row=1, column=1, padx=10, pady=10)
 
 #run
 gui.mainloop()
-
 
 
 
