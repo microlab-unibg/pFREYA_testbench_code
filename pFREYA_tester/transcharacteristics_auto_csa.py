e149e2f562017622d153ffb0eb25d1644aff7947
Paolo Lazzaroni <irockpol@gmail.com>
2024-12-19 10:33:19 +0100

    FIX - come stuff on transcharacteristcs

--- pFREYA_tester/transcharacteristics_auto_csa.py
+++ pFREYA_tester/transcharacteristics_auto_csa.py
@@ -43,7 +43,7 @@ dizionario per salvare dati di configurazione
 '''
 dati = {
     'energy level [keV]': [],
-    'Peaking time [ns]': [],
+    'Offset [mV]': [],
     'Slope [mV/#$\\gamma$]': [],
     'INL [%]': [],
     'R$^2$': []
@@ -122,6 +122,7 @@ for item in config_bits_list:
         'iinj_int (C)': [],
         'Equivalent Photons': [],
         'Voltage output average (V)': [],
+        'Voltage output average no ofs (V)': [],
         'Voltage output std (V)': []
     }
 
@@ -146,6 +147,10 @@ for item in config_bits_list:
     if channel_name == 'csa':
         mis['Voltage output average (V)'] = [-1* x for x in mis['Voltage output average (V)']]
         #mis['Voltage output average (V)'] = -1*mis['Voltage output average (V)']
+    
+    offset = mis['Voltage output average (V)'][0]
+    mis['Voltage output average no ofs (V)'] = mis['Voltage output average (V)'] - offset
+
     df = pd.DataFrame(mis)
     datetime_str = datetime.now().strftime('%Y%m%d%H%M')
 
@@ -160,23 +165,23 @@ for item in config_bits_list:
     fig.set_figheight(4)
     fig.set_figwidth(5)
 
-    ax.errorbar(photon_span, df['Voltage output average (V)'],
+    ax.errorbar(photon_span, df['Voltage output average no ofs (V)'],
                 xerr=np.tile(1, df.shape[0]), yerr=df['Voltage output std (V)'],
                 fmt='s', markersize=1, capsize=3)
     ax.set_xlabel('Equivalent input photons [#]')
-    ax.set_ylabel(f'{channel_name.upper()} output voltage [V]')
+    ax.set_ylabel(f'{channel_name.upper()} output voltage, no offset [V]')
     ax.tick_params(right=True, top=True, direction='in')
     from scipy.stats import linregress
-    ln = linregress(photon_span, df['Voltage output average (V)'].astype(float))
+    ln = linregress(photon_span, df['Voltage output average no ofs (V)'].astype(float))
     linear_output = ln.intercept + ln.slope * np.linspace(0, 256, 20)
     ax.plot(photon_span, linear_output)
-    max_diff = np.max(df['Voltage output average (V)'] - linear_output)
+    max_diff = np.max(df['Voltage output average no ofs (V)'] - linear_output)
     inl = 100 * np.abs(max_diff) / ln.slope / 256
     print(ln)
 
     ax.table(cellText=[
         ['$\\gamma$ energy [keV]', f'{config.photon_energy}'],  # Sostituisci N/A con {config.photon_energy}
-        #['Peaking time [ns]', f'{config.peaking_time}'],  # sostituisci 318 con {config.peaking_time}
+        ['Offset [mV]', f'{np.round(offset*10**3,3)}'],  # sostituisci 318 con {config.peaking_time}
         ['Slope [mV/#$\\gamma$]', f'{np.round(ln.slope*10**3,3)}'],
         ['INL [%]', f'{np.round(inl, 2)}'],
         ['R$^2$', f'{np.round(ln.rvalue**2, 3)}']
@@ -185,7 +190,7 @@ for item in config_bits_list:
 
     #salvataggio valori misurati su nel dataframe dati, in maniera che li salvi in un .tsv
     dati['energy level [keV]'].append(f'{config.photon_energy}')
-    dati['Peaking time [ns]'].append(f'{config.peaking_time}')
+    dati['Offset [ns]'].append(f'{np.round(offset*10**3,3)}')
     dati['Slope [mV/#$\\gamma$]'].append(f'{np.round(ln.slope*10**3,3)}')
     dati['INL [%]'].append(f'{np.round(inl, 2)}')
     dati['R$^2$'].append(f'{np.round(ln.rvalue**2, 3)}')
@@ -225,7 +230,7 @@ photon_span = np.linspace(0, 256, 20)
 for i in range(4):
     ax.errorbar(
         photon_span,
-        dfs[i]['Voltage output average (V)'],
+        dfs[i]['Voltage output average no ofs (V)'],
         xerr=np.tile(1, dfs[i].shape[0]),
         yerr=dfs[i]['Voltage output std (V)'],
         fmt='s',
@@ -246,6 +251,7 @@ inls = []
 #dizionario per risultati totali
 data_summary ={
     'Mode [keV]': [],
+    'Offset [mV]': [],
     'Gain [mV/γ]': [],
     'Gain [mV/fC]': [],
     'INL [%]': []
@@ -255,7 +261,7 @@ for i in range(4):
     from scipy.stats import linregress
     lns.append(linregress(
         photon_span,
-        dfs[i]['Voltage output average (V)'].astype(float)
+        dfs[i]['Voltage output average no ofs (V)'].astype(float)
     ))
     linear_outputs.append(lns[i].intercept + lns[i].slope * np.linspace(0, 256, 20))
     ax.plot(
@@ -263,11 +269,13 @@ for i in range(4):
         linear_outputs[i],
         color=colours[i]
     )
-    max_diffs.append(np.max(dfs[i]['Voltage output average (V)'] - linear_outputs[i]))
+    max_diffs.append(np.max(dfs[i]['Voltage output average no ofs (V)'] - linear_outputs[i]))
     inls.append(100 * np.abs(max_diffs[i]) / lns[i].slope / 256)
+    offset = dfs[i]['Voltage output average (V)'][0]
 
     #per ogni configurazione di KeV salvo dati
     data_summary['Mode [keV]'].append(f'{modes[i]}')
+    data_summary['Offset [mV]'].append(f'{np.round(offset*10**3, 3)}')
     data_summary['Gain [mV/γ]'].append(f'{np.round(lns[i].slope*10**3, 3)}')
     data_summary['Gain [mV/fC]'].append(f'{np.round(lns[i].slope*10**3/modes[i]*config.conv_kev_c*10**-15, 3)}')
     data_summary['INL [%]'].append(f'{np.round(inls[i], 2)}')
@@ -277,6 +285,7 @@ summary.to_csv(f'G:/Shared drives/FALCON/measures/new/transcharacteristics/csa/s
 
 ax.table(cellText=[
     ['Mode [keV]', f'{modes[0]}', f'{modes[1]}', f'{modes[2]}', f'{modes[3]}'],
+    ['Mode [keV]', f'{data_summary['Offset [mV]'][0]}', f'{data_summary['Offset [mV]'][1]}', f'{data_summary['Offset [mV]'][2]}', f'{data_summary['Offset [mV]'][3]}'],
     ['Gain [mV/γ]', f'{np.round(lns[0].slope*10**3, 3)}', f'{np.round(lns[1].slope*10**3, 3)}', f'{np.round(lns[2].slope*10**3, 3)}', f'{np.round(lns[3].slope*10**3, 3)}'],
     ['Gain [mV/fC]', f'{np.round(lns[0].slope*10**3/modes[0]*config.conv_kev_c*10**-15, 3)}', f'{np.round(lns[1].slope*10**3/modes[1]*config.conv_kev_c*10**-15, 3)}', f'{np.round(lns[2].slope*10**3/modes[2]*config.conv_kev_c*10**-15, 3)}', f'{np.round(lns[3].slope*10**3/modes[3]*config.conv_kev_c*10**-15, 3)}'],
     ['INL [%]', f'{np.round(inls[0], 2)}', f'{np.round(inls[1], 2)}', f'{np.round(inls[2], 2)}', f'{np.round(inls[3], 2)}'],
