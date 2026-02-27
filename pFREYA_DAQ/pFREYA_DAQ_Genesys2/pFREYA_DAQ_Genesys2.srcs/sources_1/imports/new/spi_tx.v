216c30fbf15fb0141893ce5f4d02499b91c0fe1b
Gabriele Chignoli <gabrielechignoli2001@gmail.com>
2026-02-27 12:39:46 +0100

    Update spi_tx.v
    Work on state machine

--- pFREYA_DAQ/pFREYA_DAQ_Genesys2/pFREYA_DAQ_Genesys2.srcs/sources_1/imports/new/spi_tx.v
+++ pFREYA_DAQ/pFREYA_DAQ_Genesys2/pFREYA_DAQ_Genesys2.srcs/sources_1/imports/new/spi_tx.v
@@ -1,18 +1,18 @@
-// Codice per la dichiarazione del modulo dell'SPI per comunicare con il DAC 
+// Code to implement behavior of transmission via SPI to DAC 
 
 module spi_tx
     #(parameter CKS_PER_BIT=2)
     (
-        // Internal clock at 10 MHz
+        // Internal clock at 10 MHz, at least 2X SPI clock
         input i_Clk,
         input [15:0] i_Tx_Data,
         // Input Data Valid
         input i_Tx_DV,   
         
         // Output clock to the DAC at 5 MHz
-        output o_SPI_Sclk,
-        output o_SPI_Clr,
-        output o_SPI_Cs,
+        output o_SPI_Sclk,  // To establish bit reading by the DAC
+        output o_SPI_Clr,   // To reset
+        output o_SPI_Cs,    // To establish transmission
         output o_SPI_Din    // For serial transmission
     );
 
@@ -22,8 +22,10 @@ module spi_tx
 
     // State Machine status for main transmission (4 states) 
     reg [1:0]   r_SM_Main = 0;
-    reg [3:0]   r_Bit_Count = 0;
+    reg [3:0]   r_Bit_Count = 16;
     reg [15:0]  r_Data_Local = 0;
+    reg [3:0]   r_Data_Index = 0;
+    reg [1:0]   r_Clock_Count = 0;
     
     wire        w_Master_Ready;
 
@@ -37,8 +39,11 @@ module spi_tx
 
                 if( i_Tx_DV == 1'b1 )
                 begin
-                    r_Data_Local <= i_Tx_Data;
-                    r_SM_Main <= TRANSFER; 
+                    // Copy for guaranteed local data integrity 
+                    r_Data_Local    <= i_Tx_Data;
+                    // So that CS is already low on next posedge
+                    o_SPI_Cs        <= 1'b0;
+                    r_SM_Main       <= TRANSFER; 
                 end
                 else
                     r_SM_Main <= s_IDLE;
@@ -48,7 +53,16 @@ module spi_tx
             begin
                 if( w_Master_Ready )
                 begin
-                    o_SPI_Cs <= 1'b1;
+                    if ( r_Clock_Count < CKS_PER_BIT )
+                    begin
+                        o_SPI_Din       <= r_Data_Local[r_Data_Index];
+                        r_Clock_Count   <= r_Clock_Count + 1'b1;
+                    end
+                    else 
+                    begin
+                        r_Bit_Count     <= r_Bit_Count - 1'b1;
+                        r_Clock_Count   <= 1'b0;  
+                    end 
                 end 
             end 
         default: 
